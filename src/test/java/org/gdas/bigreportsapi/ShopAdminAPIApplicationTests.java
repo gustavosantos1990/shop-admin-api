b5283ee4bdb4d3a29d22161b2619d97876042f76
Gustavo de Almeida Santos <gustavoa.santos90@gmail.com>
2023-10-02 20:26:06 -0300

    fix basic context test

--- src/test/java/org/gdas/bigreportsapi/ShopAdminAPIApplicationTests.java
+++ src/test/java/org/gdas/bigreportsapi/ShopAdminAPIApplicationTests.java
@@ -1,9 +1,13 @@
 package org.gdas.bigreportsapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = ShopAdminAPIApplication.class)
+@ActiveProfiles("TESTING")
+//@DataJpaTest
 class ShopAdminAPIApplicationTests {
 
 	@Test
